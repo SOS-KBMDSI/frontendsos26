d740ceccf3c0cec8f7917339238dc4ab3c3efb57
syaquiii <msyauqioqi@gmail.com>
2025-07-13 21:49:24 +0700

    refactor: adjusting responsive

--- src/feature/(user)/aktivitas/components/AktivitasAfterLogin.tsx
+++ src/feature/(user)/aktivitas/components/AktivitasAfterLogin.tsx
@@ -12,63 +12,69 @@ interface AktivitasAfterLoginProps {
 
 const AktivitasAfterLogin = ({ user }: AktivitasAfterLoginProps) => {
   return (
-    <div className="w-screen  flex flex-col min-h-screen relative  pt-20 lg:pt-40">
-      <div className="flex  mycontainer flex-col-reverse lg:flex-row   items-center">
-        <div className="bg-secondary-100 w-full border rounded-2xl lg:w-[800px] xl:w-[1000px] z-20 border-secondary-700 p-6">
-          <div className="bg-primary-500 p-5 rounded-2xl text-white">
-            <h4 className="lg:text-3xl xl:text-4xl">Fasilitator</h4>
-            <div className="grid grid-cols-2 lg:text-xl xl:text-2xl mt-3">
-              {user.kelompok?.distrik?.list_pjl.map((pjl, idx) => (
-                <span key={idx}>
-                  {pjl.nama} - {pjl.line}
-                </span>
-              ))}
+    <div className="w-screen   flex flex-col min-h-screen relative  pt-20 lg:pt-40">
+      <div className="content-container ">
+        <div className="flex  justify-between flex-col-reverse md:flex-row ">
+          <div className="bg-secondary-100 h-fit w-full border rounded-2xl md:w-[1000px] xl:w-[1000px] z-20 border-secondary-700 p-6">
+            <div className="bg-primary-500 p-5 rounded-2xl text-white">
+              <h4 className="lg:text-3xl xl:text-4xl">Fasilitator</h4>
+              <div className="grid grid-cols-2 lg:text-xl xl:text-2xl mt-3">
+                {user.kelompok?.distrik?.list_pjl.map((pjl, idx) => (
+                  <span key={idx}>
+                    {pjl.nama} - {pjl.line}
+                  </span>
+                ))}
+              </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-y-8 p-5">
+              <div>
+                <p className="text-primary-500 lg:text-xl xl:text-2xl font-medium">
+                  Nama Lengkap
+                </p>
+                <p className="lg:text-lg xl:text-xl text-black mt-1">
+                  Nama Lengkap
+                </p>
+              </div>
+              <div>
+                <p className="text-primary-500 lg:text-xl xl:text-2xl font-medium">
+                  Distrik
+                </p>
+                <p className="lg:text-lg xl:text-xl text-black mt-1">
+                  {user.kelompok?.distrik?.nama_distrik}
+                </p>
+              </div>
+              <div>
+                <p className="text-primary-500 lg:text-xl xl:text-2xl font-medium">
+                  Program Studi
+                </p>
+                <p className="lg:text-lg xl:text-xl text-black mt-1">
+                  {user.prodi}
+                </p>
+              </div>
+              <div>
+                <p className="text-primary-500 lg:text-xl xl:text-2xl font-medium">
+                  Kelompok
+                </p>
+                <p className="lg:text-lg xl:text-xl text-black mt-1">
+                  {user.kelompok?.nama_kelompok}
+                </p>
+              </div>
+              <div>
+                <p className="text-primary-500 lg:text-xl xl:text-2xl font-medium">
+                  NIM
+                </p>
+                <p className="lg:text-lg xl:text-xl text-black mt-1">
+                  {user.nim}
+                </p>
+              </div>
             </div>
           </div>
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-y-8 p-5">
-            <div>
-              <p className="text-primary-500 lg:text-xl xl:text-2xl font-medium">
-                Nama Lengkap
-              </p>
-              <p className="lg:text-lg xl:text-xl text-black mt-1">
-                Nama Lengkap
-              </p>
-            </div>
-            <div>
-              <p className="text-primary-500 lg:text-xl xl:text-2xl font-medium">
-                Distrik
-              </p>
-              <p className="lg:text-lg xl:text-xl text-black mt-1">
-                {user.kelompok?.distrik?.nama_distrik}
-              </p>
-            </div>
-            <div>
-              <p className="text-primary-500 lg:text-xl xl:text-2xl font-medium">
-                Program Studi
-              </p>
-              <p className="lg:text-lg xl:text-xl text-black mt-1">
-                {user.prodi}
-              </p>
-            </div>
-            <div>
-              <p className="text-primary-500 lg:text-xl xl:text-2xl font-medium">
-                Kelompok
-              </p>
-              <p className="lg:text-lg xl:text-xl text-black mt-1">
-                {user.kelompok?.nama_kelompok}
-              </p>
-            </div>
-            <div>
-              <p className="text-primary-500 lg:text-xl xl:text-2xl font-medium">
-                NIM
-              </p>
-              <p className="lg:text-lg xl:text-xl text-black mt-1">
-                {user.nim}
-              </p>
-            </div>
-          </div>
+          <Image
+            className="md:-ml-30 md:mr-30 md:w-1/3 z-30"
+            src={Maskor2}
+            alt="Maskot"
+          />
         </div>
-        <Image className="lg:-ml-40 lg:w-1/3 z-30" src={Maskor2} alt="Maskot" />
       </div>
       <AktivitasBackground />
       <Image
