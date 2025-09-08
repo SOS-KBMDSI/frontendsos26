4192636dfe2cccfc702a2ee3df8d4597282c28b4
syaquiii <msyauqioqi@gmail.com>
2025-09-08 21:19:50 +0700

    refactor: quiz card and tugas card

--- src/feature/(admin)/penugasan/tugas/components/TugasCard.tsx
+++ src/feature/(admin)/penugasan/tugas/components/TugasCard.tsx
@@ -11,9 +11,11 @@ interface TugasCardProps {
 
 export const TugasCard: React.FC<TugasCardProps> = ({ tugas, idx }) => {
   return (
-    <div className="bg-gray-200/50 hover:cursor-pointer  flex justify-between rounded-lg p-4">
-      <div className="text-xl text-black">
-        Tugas {idx} : ({tugas.judul})
+    <div className="bg-gray-200/50 hover:cursor-pointer  flex justify-between items-center rounded-lg p-4">
+      <div className="text-2xl  items-center text-black flex gap-2">
+        <span className="font-medium">Tugas {idx}</span>
+        <span>:</span>
+        <span className="text-xl">({tugas.judul})</span>
       </div>
       <div>
         <Ellipsis className="rotate-90" />
