99182649509e80fcc16df593913722ead13c4692
syaquiii <msyauqioqi@gmail.com>
2025-09-12 00:43:07 +0700

    feat: removing dummy data

--- src/feature/(user)/home/data/memori.ts
+++ src/feature/(user)/home/data/memori.ts
@@ -12,31 +12,31 @@ export const memoriData: Memori[] = [
   {
     id: 1,
     content: "Launching Soon 1",
-    title: "The Grand Voyage",
+    title: "Soon",
   },
   {
     id: 2,
     content: "Launching Soon 2",
-    title: "Symphonic Dreams",
+    title: "Soon",
   },
   {
     id: 3,
     content: "launching soon 3",
-    title: "Mystery Adventure",
+    title: "Soon",
   },
   {
     id: 4,
     content: "launching soon 4",
-    title: "Epic Quest",
+    title: "Soon",
   },
   {
     id: 5,
     content: "launching soon 5",
-    title: "Hidden Treasures",
+    title: "Soon",
   },
   {
     id: 6,
     content: "launching soon 6",
-    title: "OTW",
+    title: "Soon",
   },
 ];
