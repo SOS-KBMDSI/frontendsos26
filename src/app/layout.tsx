30047234626a011d202eb88f1c03598deb570857
syaquiii <msyauqioqi@gmail.com>
2025-09-12 15:11:53 +0700

    feat: adding logo on seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,15 @@ import type { Metadata } from "next";
 import "@/styles/globals.css";
 import { upanddownNormal, poppins } from "@/shared/utils/font";
 import { defaultMetadata } from "@/shared/utils/metadata";
-
-export const metadata: Metadata = defaultMetadata;
+import SOSLOGO from "@/assets/logo-sos.png";
+export const metadata: Metadata = {
+  ...defaultMetadata,
+  icons: {
+    icon: SOSLOGO.src,
+    shortcut: SOSLOGO.src,
+    apple: SOSLOGO.src,
+  },
+};
 
 export default function RootLayout({
   children,
@@ -12,6 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="id">
+      <head>
+        <link rel="icon" type="image/png" href={SOSLOGO.src} />
+        <link rel="shortcut icon" type="image/png" href={SOSLOGO.src} />
+        <link rel="apple-touch-icon" href={SOSLOGO.src} />
+      </head>
       <body
         className={`${poppins.variable} ${upanddownNormal.variable} font-poppins antialiased`}
       >
